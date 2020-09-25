f19e47ee3fd9a2778e60537de03927deab2012e1
IPostYellow <32611516+IPostYellow@users.noreply.github.com>
2020-09-25 11:00:37 +0800

    Add files via upload
    增加了一个实现获取最小值的方法

--- Basic_data_structure/包含最小值的栈.py
+++ Basic_data_structure/包含最小值的栈.py
@@ -1,4 +1,4 @@
-class Stack:
+class Stack:  # 基础堆栈
     def __init__(self):
         self.__list = []
 
@@ -21,7 +21,7 @@ class Stack:
             return self.__list[-1]
 
 
-class Stack_with_min:
+class Stack_with_min:  # 拥有栈内最小值信息的栈，进栈容易，出栈麻烦
     def __init__(self):
         self.stackdata = Stack()
         self.stackmin = Stack()
@@ -33,14 +33,60 @@ class Stack_with_min:
         self.stackdata.push(item)
         if self.stackmin.is_empty():
             self.stackmin.push(item)
-        if self.stackmin.top() >= item:
+        elif self.stackmin.top() >= item:
             self.stackmin.push(item)
 
     def pop(self):
-        if self.stackdata.top() == self.stackmin.top():
-            self.stackmin.pop()
-            return self.stackdata.pop()
+        if self.stackdata.is_empty():
+            return
         else:
+            if self.stackdata.top() == self.stackmin.top():
+                self.stackmin.pop()
+                return self.stackdata.pop()
+            else:
+                return self.stackdata.pop()
+
+    def top(self):
+        return self.stackdata.top()
+
+    def get_min(self):
+        return self.stackmin.top()
+
+    def print_stack(self):
+        stack1 = self.stackdata
+        stack2 = self.stackmin
+        list1 = []
+        list2 = []
+        while (stack1.is_empty() == False):
+            list1.append(stack1.pop())
+        while (stack2.is_empty() == False):
+            list2.append(stack2.pop())
+        print('stack_data:', list1)
+        print('stack_min:', list2)
+
+
+class Stack_with_min2:  # 拥有栈内最小值信息的栈，进栈麻烦，出栈容易
+    def __init__(self):
+        self.stackdata = Stack()
+        self.stackmin = Stack()
+
+    def is_empty(self):
+        return self.stackdata.is_empty()
+
+    def push(self, item):
+        self.stackdata.push(item)
+        if self.stackmin.is_empty():
+            self.stackmin.push(item)
+        elif self.stackmin.top() >= item:
+            self.stackmin.push(item)
+        else:
+            self.stackmin.push(self.stackmin.top())
+
+    def pop(self):
+        if self.stackdata.is_empty():
+            return
+        else:
+            self.stackmin.pop()
             return self.stackdata.pop()
 
     def top(self):
@@ -49,4 +95,39 @@ class Stack_with_min:
     def get_min(self):
         return self.stackmin.top()
 
+    def print_stack(self):
+        stack1 = self.stackdata
+        stack2 = self.stackmin
+        list1 = []
+        list2 = []
+        while (stack1.is_empty() == False):
+            list1.append(stack1.pop())
+        while (stack2.is_empty() == False):
+            list2.append(stack2.pop())
+        print('stack_data:', list1)
+        print('stack_min:', list2)
 
+
+# s=Stack_with_min()
+# s.push(1)
+# s.push(1)
+# s.push(5)
+# s.push(4)
+# s.push(7)
+# s.push(0)
+# s.push(1)
+# s.push(9)
+# s.push(0)
+# s.print_stack()
+
+s = Stack_with_min2()
+s.push(1)
+s.push(1)
+s.push(5)
+s.push(4)
+s.push(7)
+s.push(0)
+s.push(1)
+s.push(9)
+s.push(0)
+s.print_stack()
