c72a8701487b5c7e6bbd826de263fc5cd6d9f79e
君名逍遥难逍遥 <185184970@qq.com>
2020-11-27 16:58:04 +0800

    39组合总数

--- 回溯/python/46全排列.py
+++ 回溯/python/46全排列.py
@@ -18,13 +18,14 @@
 链接：https://leetcode-cn.com/problems/permutations
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
+import copy
 from typing import List
 
 
 class Solution:  # 44ms,13.8MB
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
 
+        result = []
         def backtrack(nums, tmp):
             if not nums:  # 结束条件。也就是得到n个数字以后
                 result.append(tmp)
@@ -39,3 +40,82 @@ class Solution:  # 44ms,13.8MB
 
 s = Solution()
 print(s.permute([1, 2, 3]))
+
+
+class Solution2:  # 44ms,13.8MB
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        nums=sorted(nums)
+        result = []
+        used = []
+        for i in nums:
+            used.append(False)
+
+        def backtrack(nums, used, tmp):
+            if len(nums) == len(tmp):
+                result.append(copy.deepcopy(tmp))
+                return
+            for i in range(len(nums)):
+                if (used[i]) or (i > 0 and used[i - 1]==False and nums[i] == nums[i - 1]):
+                    print("used[i]",i,used[i],"used[i-1]",used[i-1],"tmp",tmp)
+                    continue
+                tmp.append(nums[i])
+                used[i] = True
+                backtrack(nums, used, tmp)
+                used[i] = False
+                tmp.pop(-1)
+            # 每次取去掉那个i的数，然后将其数拼起来，tmp就是已经拼好的
+
+        backtrack(nums, used, [])
+        return result
+
+
+s = Solution2()
+print(s.permute([1, 1, 2]))
+
+# import copy
+# class Solution3:
+#     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+#         self.result=[]
+#         used=[]
+#         for j in nums:
+#             used.append(False)
+#
+#         self.trackback(nums,used,[])
+#         return self.result
+#     def trackback(self,nums,used,tmp):
+#         if len(nums)==len(tmp):
+#             self.result.append(copy.deepcopy(tmp))
+#             return
+#         for i in range(len(nums)):
+#             if (used[i]):
+#                 continue
+#             if(i>0  and used[i]==used[i-1] and used[i-1]==False):
+#                 continue
+#             used[i]=True
+#             self.trackback(nums,used,tmp+[nums[i]])
+#             used[i]=False
+
+
+
+class Solution4:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        self.res = []
+        check = [0 for i in range(len(nums))]
+
+        self.backtrack([], nums, check)
+        return self.res
+
+    def backtrack(self, sol, nums, check):
+        if len(sol) == len(nums):
+            self.res.append(sol)
+            return
+
+        for i in range(len(nums)):
+            if check[i] == 1:
+                continue
+            if i > 0 and nums[i] == nums[i - 1] and check[i - 1] == 0:
+                continue
+            check[i] = 1
+            self.backtrack(sol + [nums[i]], nums, check)
+            check[i] = 0
