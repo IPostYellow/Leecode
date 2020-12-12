82a57f30ad56acf1b18c337b003ba5277d48e915
IPostYellow <185184970@qq.com>
2020-12-12 09:36:54 +0800

    300

--- 动态规划/python/300.最长上升子序列.py
+++ 动态规划/python/300.最长上升子序列.py
@@ -40,7 +40,9 @@ print(s.lengthOfLIS([1, 3, 6, 7, 9, 4, 10, 5, 6]))
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 '''
-class Solution2:#60ms,13.7mb
+
+
+class Solution2:  # 60ms,13.7mb
     def lengthOfLIS(self, nums: List[int]) -> int:
         tmp = [nums[0]]
         for i in nums[1:]:
@@ -52,10 +54,52 @@ class Solution2:#60ms,13.7mb
                     mid = left + (right - left >> 1)
                     if i > tmp[mid]:
                         left = mid + 1
-                    elif i<tmp[mid]:
+                    elif i < tmp[mid]:
                         right = mid
                     else:
-                        left=mid
+                        left = mid
                         break
-                tmp[left]=i
+                tmp[left] = i
         return len(tmp)
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class Solution3:  # 只通过了15个例子
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        first = TreeNode(nums[0])
+        p1 = first
+        tmp = [p1]
+        for i in range(1, len(nums)):
+            for j in range(len(tmp)):
+                if nums[i] > tmp[j].val:
+                    tmp[j].right = TreeNode(nums[i])
+                    tmp[j] = tmp[j].right
+                else:
+                    tmp[j].left = TreeNode(nums[i])
+                    p2 = tmp[j].left
+                    tmp.append(p2)
+
+        def finddeepth(root):
+            if root == None:
+                return 0
+            r = finddeepth(root.right) + 1
+            return r
+
+        p=[first]
+        maxd=0
+        while(p!=[]):
+            tnode=p.pop()
+            l=finddeepth(tnode)
+            if l>maxd:
+                maxd=l
+            if tnode.left:
+                p.append(tnode.left)
+            if tnode.right:
+                p.append(tnode.right)
+        return maxd
