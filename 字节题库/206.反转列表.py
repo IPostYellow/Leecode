6287da3a061838a0c9e023f0da367787e2ca0f7e
IPostYellow <185184970@qq.com>
2020-12-13 08:40:24 +0800

    new

---
+++ 字节题库/206.反转列表.py
@@ -0,0 +1,61 @@
+'''
+反转一个单链表。
+示例:
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+'''
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        first = ListNode(-1)
+        first.next = head
+        head = head.next
+        first.next.next=None
+        while (head != None):
+            tmp = head
+            head = head.next
+            tmp.next = first.next
+            first.next = tmp
+        return first.next
+
+l1=ListNode(1)
+l2=ListNode(2)
+l3=ListNode(3)
+l4=ListNode(4)
+l5=ListNode(5)
+l1.next=l2
+l2.next=l3
+l3.next=l4
+l4.next=l5
+def dayinlianbiao(s):
+    while(s!=None):
+        print(s.val)
+        s=s.next
+# s=Solution()
+# d=s.reverseList(l1)
+# dayinlianbiao(d)
+
+class Solution2:
+    def reverseList(self, head: ListNode) -> ListNode:
+        first = ListNode(-1)
+        def headinsert(first,head):
+            if head==None:
+                return
+            else:
+                tmp=head
+                head=head.next
+                tmp.next=first.next
+                first.next=tmp
+                headinsert(first,head)
+            return
+        headinsert(first,head)
+        return first.next
+
+s=Solution2()
+dayinlianbiao(s.reverseList(l1))
