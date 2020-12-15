174170dfa367acde170a588e939f6afea298cff4
君名逍遥难逍遥 <185184970@qq.com>
2020-12-15 15:33:26 +0800

    s

--- 字节题库/146.LRU缓存机制.py
+++ 字节题库/146.LRU缓存机制.py
@@ -1,24 +1,16 @@
 '''
 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制 。
 实现 LRUCache 类：
-
 LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
 int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
 void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
- 
-
 进阶：你是否可以在 O(1) 时间复杂度内完成这两种操作？
-
- 
-
 示例：
-
 输入
 ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
 [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
 输出
 [null, null, null, 1, null, -1, null, -1, 3, 4]
-
 解释
 LRUCache lRUCache = new LRUCache(2);
 lRUCache.put(1, 1); // 缓存是 {1=1}
@@ -35,11 +27,8 @@ lRUCache.get(4);    // 返回 4
 链接：https://leetcode-cn.com/problems/lru-cache
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
-
-
-class BiListNode:
-
-    def __init__(self, key, val):
+class Bilistnode:
+    def __init__(self, key, val) -> None:
         self.key = key
         self.val = val
         self.next = None
@@ -50,51 +39,53 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.curcache = 0
         self.hashmap = {}
-        self.head = BiListNode(None, None)
-        self.tail = BiListNode(None, None)
+        self.head = Bilistnode(None, None)
+        self.tail = Bilistnode(None, None)
 
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def move_to_tail(self, key):  # 将节点移动到尾端
+    def move_node_to_tail(self, key):
         tmp_node = self.hashmap[key]
-        tmp_node.next.prev = tmp_node.prev  # 断开tmp_node的后面的箭头
-        tmp_node.prev.next = tmp_node.next  # 断开tmp_node前面的箭头
-        # 如此一来，tmp_node前后的node就连起来了，把tmp_node取出来了
+        tmp_node.next.prev = tmp_node.prev
+        tmp_node.prev.next = tmp_node.next
 
-        tmp_node.next = self.tail  # 将取出的tmp_node放到tail前面，原本tail前面node的后面
         tmp_node.prev = self.tail.prev
+        tmp_node.next = self.tail
         self.tail.prev.next = tmp_node
         self.tail.prev = tmp_node
 
     def get(self, key: int) -> int:
-        if key in self.hashmap:
-            self.move_to_tail(key)
+        if key in self.hashmap.keys():
+            self.move_node_to_tail(key)
             return self.hashmap[key].val
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.hashmap:
-            self.move_to_tail(key)
+        if key in self.hashmap.keys():
             self.hashmap[key].val = value
+            self.move_node_to_tail(key)
         else:
-            if self.capacity == self.curcache:
+            if len(self.hashmap) == self.capacity:
                 self.hashmap.pop(self.head.next.key)
                 self.head.next.next.prev = self.head
                 self.head.next = self.head.next.next
-                self.curcache -= 1
-            new_node = BiListNode(key, value)
-            self.hashmap[key] = new_node
-            new_node.prev = self.tail.prev
-            new_node.next = self.tail
-            self.tail.prev.next = new_node
-            self.tail.prev = new_node
-            self.curcache += 1
+            now_node = Bilistnode(key, value)
+            self.hashmap[key] = now_node
+            self.tail.prev.next = now_node
+            now_node.next = self.tail
+            now_node.prev = self.tail.prev
+            self.tail.prev = now_node
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+s = LRUCache(2)
+print(s.put(1, 1))
+print(s.put(2, 2))
+print(s.get(1))
+print(s.put(3, 3))
+print(s.get(2))
+print(s.put(4, 4))
+print(s.get(1))
+print(s.get(3))
+print(s.get(4))
---
+++ 字节题库/681.最近时刻.py
@@ -0,0 +1,38 @@
+'''
+给定一个形如 “HH:MM” 表示的时刻，利用当前出现过的数字构造下一个距离当前时间最近的时刻。每个出现数字都可以被无限次使用。
+你可以认为给定的字符串一定是合法的。例如，“01:34” 和 “12:09” 是合法的，“1:34” 和 “12:9” 是不合法的。
+
+样例 1:
+输入: "19:34"
+输出: "19:39"
+解释: 利用数字 1, 9, 3, 4 构造出来的最近时刻是 19:39，是 5 分钟之后。结果不是 19:33 因为这个时刻是 23 小时 59 分钟之后。
+样例 2:
+输入: "23:59"
+输出: "22:22"
+解释: 利用数字 2, 3, 5, 9 构造出来的最近时刻是 22:22。 答案一定是第二天的某一时刻，所以选择可构造的最小时刻。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/next-closest-time
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+class Solution:
+    def nextClosestTime(self, time: str) -> str:
+        tmp_time = time.split(":")
+        number_list = []
+        for i in tmp_time:
+            for j in i:
+                number_list.append(int(j))
+        total_time = int(tmp_time[0]) * 60 + int(tmp_time[1])
+        for i in range(total_time + 1, 1441):
+            if (i // 600 in number_list) and (i // 60 % 10 in number_list) and (
+                    i % 60 // 10 in number_list) and (i % 60 % 10 in number_list):
+                return (str(i // 600) + str(i // 60 % 10) +":"+ str(i % 60 // 10) + str(
+                    i % 60 % 10))
+        #第二天了
+        for i in range(total_time+1):
+            if (i // 600 in number_list) and (i // 60 % 10 in number_list) and (
+                    i % 60 // 10 in number_list) and (i % 60 % 10 in number_list):
+                return (str(i // 600) + str(i // 60 % 10)+":" + str(i % 60 // 10) + str(
+                    i % 60 % 10))
+s = Solution()
+print(s.nextClosestTime("19:23"))
