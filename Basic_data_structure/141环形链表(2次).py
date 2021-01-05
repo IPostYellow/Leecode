3d8a8825cbba5285aea31ed4e4ac737b35c03928
IPostYellow <185184970@qq.com>
2021-01-05 20:59:42 +0800

    141

---
+++ Basic_data_structure/141环形链表(2次).py
@@ -0,0 +1,64 @@
+'''
+给定一个链表，判断链表中是否有环。
+如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+如果链表中存在环，则返回 true 。 否则，返回 false 。
+
+进阶：
+你能用 O(1)（即，常量）内存解决此问题吗？
+
+示例 1：
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+示例 2：
+输入：head = [1,2], pos = 0
+输出：true
+解释：链表中有一个环，其尾部连接到第一个节点。
+
+示例 3：
+输入：head = [1], pos = -1
+输出：false
+解释：链表中没有环。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/linked-list-cycle
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head==None or head.next==None:
+            return False
+        slow=head
+        fast=head.next
+        while(slow!=fast):
+            if fast==None or fast.next==None:
+                return False
+            slow=slow.next
+            fast=fast.next.next
+        return True
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution_2:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head or not head.next:
+            return False
+        p1,p2=head,head
+        while(p1!=None and p2!=None and  p2.next!=None):
+            p1=p1.next
+            p2=p2.next.next
+            if p1==p2:
+                return True
+        return False
